6f06945568c3adbb51e048e3b8ead652957dbf2b
Harsh Mishra <mishraharsh747@gmail.com>
2021-03-03 11:35:05 +0530

    'remove_classes'

--- src/Rate.jsx
+++ src/Rate.jsx
@@ -1,62 +1,49 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
-export default class Rate extends Component {
-    constructor() {
-        super();
-        this.state = {
-            userData: null,
-            handle: ''
-        };
-    }
 
-    onSubmit = (e) => {
-        e.preventDefault();
+function Rate() {
+
+    const [userData, setUserData] = useState(null);
+    const [handle, setHandle] = useState('');
+
+    function onSubmit(event) {
+        event.preventDefault();
         let data = {
-            handle: this.state.handle
+            handle: handle
         }
         axios.post("http://localhost:5000/nrating", data).then(response => {
             console.log(response.data);
-            this.setState({
-                userData: response.data
-            });
+            setUserData(response.data);
         });
     }
 
-    render() {
-        return (
-            <div>
-                {this.state.userData === null ?
-                    <form onSubmit={this.onSubmit}>
-                        <label>User Handle</label>
-                        <input id="Handle" type="text" name="userHandle" onChange={(e) => {
-                            this.setState({
-                                handle: e.target.value
-                            })
-                        }} />
-                        <button type="submit">Go</button>
-                    </form> :
-                    <div>
-                        <h1>User Rating: {this.state.userData.rating}</h1>
-                        {this.state.userData.ques.length > 0 ?
-                            <div>
-                                <h1>Questions: </h1>
-                                <h2>{this.state.userData.ques[0].contestId}  {this.state.userData.ques[0].name}</h2>
-                                <h2>{this.state.userData.ques[1].contestId}  {this.state.userData.ques[1].name}</h2>
-                                <h2>{this.state.userData.ques[2].contestId}  {this.state.userData.ques[2].name}</h2>
-                                <h2>{this.state.userData.ques[3].contestId}  {this.state.userData.ques[3].name}</h2>
-                                <h2>{this.state.userData.ques[4].contestId}  {this.state.userData.ques[4].name}</h2>
-                                <h2>{this.state.userData.ques[5].contestId}  {this.state.userData.ques[5].name}</h2>
-                                <h2>{this.state.userData.ques[6].contestId}  {this.state.userData.ques[6].name}</h2>
-                                <h2>{this.state.userData.ques[7].contestId}  {this.state.userData.ques[7].name}</h2>
-                                <h2>{this.state.userData.ques[8].contestId}  {this.state.userData.ques[8].name}</h2>
-                                <h2>{this.state.userData.ques[9].contestId}  {this.state.userData.ques[9].name}</h2>
-                            </div>
-                            : <h2>Please Enter Valid Handle</h2>
-                        }
-                    </div>
-                }
-            </div>
-        );
+    function show(e) {
+        return (<a href={"https://codeforces.com/problemset/problem/" + e.contestId + "/" + e.index}><h2>{e.contestId}  {e.name}</h2></a>);
     }
+
+    return (
+        <div>
+            {userData === null ?
+                <form >
+                    <label>User Handle</label>
+                    <input id="Handle" type="text" name="userHandle" onChange={(e) => {
+                        setHandle(e.target.value);
+                    }} />
+                    <button type="submit" onClick={onSubmit}>Go</button>
+                </form> :
+                <div>
+                    <h1>User Rating: {userData.rating}</h1>
+                    {userData.ques.length > 0 ?
+                        <div>
+                            <h1>Questions: </h1>
+                            {userData.ques.map(show)}
+                        </div>
+                        : <h2>Please Enter Valid Handle</h2>
+                    }
+                </div>
+            }
+        </div>
+    );
 }
 
+export default Rate;
