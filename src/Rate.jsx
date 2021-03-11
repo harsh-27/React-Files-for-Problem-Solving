ae3e70500ddaa91d2de2a788f664e8c387b9c254
Harsh Mishra <mishraharsh747@gmail.com>
2021-03-11 14:58:57 +0530

    'all_button_funcatinality_added'

--- src/Rate.jsx
+++ src/Rate.jsx
@@ -16,37 +16,51 @@ function Rate() {
         name: '',
         contestId: ''
     });
-    const [lo, setLogout] = useState(false);
+    //const [lo, setLogout] = useState(false);
+    const [toDoList, setToDoList] = useState(null);
 
-    function onSubmit(event) {
+
+    const left = {
+        float: "left",
+        width: "50%"
+    };
+    async function onSubmit(event) {
         event.preventDefault();
+        var res = await callData(userName, password, handle);
+        sessionStorage.setItem("userD", JSON.stringify(res.userD));
+        sessionStorage.setItem("handle", res.handle);
+        setUserData(res.userD);
+        setHandle(res.handle);
+        toogleSet(true);
+    }
+
+    function callData(userName, password, handle) {
         let data = {
-            userName: userName,
-            password: password,
-            handle: handle
+            userName,
+            password,
+            handle
         }
-        axios.post("http://localhost:5000/nrating", data).then(response => {
-            console.log(response.data);
-            setUserData(response.data);
-            sessionStorage.setItem("userD", JSON.stringify(response.data));
-            sessionStorage.setItem("handle", handle);
-            return true;
-        }).then((z) => {
-            if (z)
-                toogleSet(true);
+        // var res = null;
+        var res = axios.post("http://localhost:5000/nrating", data).then(response => {
+            res = {
+                userD: (response.data),
+                handle: handle
+            };
+        }).then(() => {
+            return res;
         })
+        return res;
     }
 
     useEffect(() => {
-        console.log("re-render because todoQues changed:", todoQues)
         let quesData = {
             name: todoQues.name,
             contestId: todoQues.contestId,
             handle: handle
         }
-        if (quesData.todoQues != '') {
+        if (quesData.name !== '') {
             axios.post("http://localhost:5000/list", quesData).then(response => {
-                console.log(response.quesData);
+                setToDoList(response.data);
             })
         }
     }, [todoQues])
@@ -54,33 +68,33 @@ function Rate() {
     useEffect(() => {
         if (window.sessionStorage.getItem('userD')) {
             setUserData(JSON.parse(window.sessionStorage.getItem('userD')));
-            setHandle(window.sessionStorage.getItem("handle"));
-            toogleSet(true);
+            setHandle(window.sessionStorage.getItem('handle'));
         }
-
-    }, userData);
+    }, []);
 
     useEffect(() => {
-        if (lo) {
-            console.log("in useefect");
-            sessionStorage.clear();
-            setUserData(null);
-            setHandle('');
-            toogleSet(false);
-            setLogout(false);
-        }
-    });
+        if (userData) {
+            let quesData = {
+                name: "",
+                contestId: "",
+                handle: handle
+            }
+            axios.post("http://localhost:5000/list", quesData).then(response => {
+                setToDoList(response.data);
+            }).then(() => {
+                toogleSet(true);
+            })
+        };
+    }, [userData]);
 
     useEffect(() => {
-        console.log("re-render because removeQues changed:", removeQues)
         let quesData = {
             name: removeQues.name,
             contestId: removeQues.contestId,
             handle: handle
         }
-        if (quesData.removeQues != '') {
+        if (quesData.contestId !== '') {
             axios.post("http://localhost:5000/uwlist", quesData).then(response => {
-                console.log(response.quesData);
             })
         }
     }, [removeQues]);
@@ -91,7 +105,6 @@ function Rate() {
             name: document.getElementById(event.target.id).id,
             contestId: document.getElementById(event.target.id).value
         });
-        console.log(document.getElementById(event.target.id).id);
         document.getElementById(event.target.id).disabled = true;
         document.getElementById(event.target.value).disabled = true;
     }
@@ -102,25 +115,51 @@ function Rate() {
             name: document.getElementById(event.target.id).value,
             contestId: document.getElementById(event.target.id).id
         });
-        console.log(document.getElementById(event.target.id).value);
         document.getElementById(event.target.id).disabled = true;
         document.getElementById(event.target.value).disabled = true;
     }
 
-    function show(e) {
-        return (<div><a href={"https://codeforces.com/problemset/problem/" + e.contestId + "/" + e.index}>
-            <h2>{e.contestId}</h2>
-            <h2 >{e.name}</h2></a>
+    function show(e, index) {
+        return (<div key={index.toString()}><a href={"https://codeforces.com/problemset/problem/" + e.contestId + "/" + e.index}>
+            <h2>{e.contestId + e.index} {e.name}</h2>
+        </a>
             <button id={e.name} value={e.contestId + e.index} onClick={onclick}>ADD</button>
             <button id={e.contestId + e.index} value={e.name} onClick={handleclick}>REMOVE</button>
         </div>);
     }
 
     function logout() {
-        console.log("logout button pressed!");
-        setLogout(true);
+        //setLogout(true);
+        sessionStorage.clear();
+        setUserData(null);
+        setHandle('');
+        toogleSet(false);
+        //setLogout(false);
+    }
+    function contest(contestId) {
+        //return "hello";
+        var n = contestId.length;
+        var str1 = '', str2 = '', i;
+        for (i = 0; i < n; i++) {
+            if (contestId[i] >= 0 && contestId[i] <= 9)
+                str1 += contestId[i];
+            else
+                break;
+        }
+        for (; i < n; i++)
+            str2 += contestId[i];
+
+        return str1 + '/' + str2;
+
     }
 
+    function showToDoList(event, index) {
+        return (<div key={index.toString()}>
+            <a href={"https://codeforces.com/problemset/problem/" + contest(event.contestId)}>
+                <h2>{event.contestId} {event.name}</h2>
+            </a>
+        </div>)
+    }
 
     return (
         <div>
@@ -142,16 +181,25 @@ function Rate() {
                 </form> :
                 <div>
                     <h1>User Rating: {userData.rating}</h1>
+
                     {userData.rating >= 0 ?
                         <div>
                             {userData.ques == 0 ?
                                 <h1>You Have not solved any question Yet</h1>
                                 : <div>
-                                    <h1>Questions: </h1>
-                                    {userData.ques.map(show)}
+                                    <div style={left}>
+                                        <h1>Questions: </h1>
+                                        {userData.ques.map(show)}
+                                    </div>
+                                    <div style={left}>
+                                        <h1>To Do List</h1>
+                                        {toDoList != null ? toDoList.map(showToDoList) : null}
+                                    </div>
                                 </div>
+
+
                             }
-                            <button onClick={logout}>Logout</button>
+                            <button onClick={logout} >Logout</button>
                         </div>
                         : <h2>Please Enter Valid Handle</h2>
                     }
